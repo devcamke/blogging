e38542db34ca5475d0929afd523ee96c36313e08
Dennis Kamau <devjam.ke@gmail.com>
2022-09-26 23:03:05 +0300

    Add like and unlike feature to the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 	has_rich_text :comment
 	has_many :comments, dependent: :destroy
+	has_many :likes, dependent: :destroy
 	validates_presence_of :title
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   resources :posts do
     resources :comments
+    resources :likes
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
